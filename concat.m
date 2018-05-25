4be33d2ea288bacf8700b070e040d142165efda7
LarsanAroBrian <39196219+LarsanAroBrian@users.noreply.github.com>
2018-05-24 22:40:50 -0700

    you might have to use MAT-file 7.3 if ur file system does not support

---
+++ concat.m
@@ -0,0 +1,12 @@
+clc; clear all; close all;
+sam_mat=["presamples_1" "presamples_2" "presamples_3" "presamples_4" ...
+    "presamples_5" "presamples_6" "presamples_7" "presamples_8" "presamples_9"]; 
+labels_new=[];
+presample_data_new=[];
+for i=1:9
+i
+load(sam_mat(i));
+labels_new=[labels_new labels];
+presample_data_new=[presample_data_new presample_data];
+end
+save('concat_presamples.mat','presample_data_new','labels_new');
