b8770438b69405447276afacd7070d911cd7a4b6
yoojeenwoo <eugenewu0@gmail.com>
2018-05-26 15:40:29 -0700

    Added more to recursive presample, integrated pruning

--- presample_recursive.m
+++ presample_recursive.m
@@ -9,6 +9,10 @@ N_PRESAMPLE = log(N)/log(BATCH_SZ)*BATCH_SZ;
 param_names = ['toxe'; 'xl  '; 'xw  '; 'vth0'; 'u0  '; 'voff'];
 hspice_path = '/w/apps3/Synopsys/HSPICE/vG-2012.06/hspice/bin/hspice';
 
+%% PRUNING STAGE
+data = load('concat_presamples.mat');
+idxs = pruning(data.labels_new, data.presample_data_new);
+
 %% PRESAMPLE STAGE
 tic
 
@@ -26,6 +30,8 @@ while n < N
 	% Sample_Gen output is organized in vertically stacked 60 x 6 blocks
 	% We reshape each block to be a 360 x 1 column of the presample_data matrix
 	% Reshape stacks each of the six 60x1 columns of the original sample
+    
+    % Select the worst delay times out of the simulation
     [~, I] = sort(td, 'descend');
     td_sorted = td(I);
     td = td_sorted(1:BATCH_SZ);
@@ -34,18 +40,19 @@ while n < N
     labels_sorted = labels(I);
     labels = labels_sorted(1:BATCH_SZ);
     
-    CLASS_THR = td(30); % 97th percentile
+    CLASS_THR = td(30); % Reset classification threshold to 97th percentile
     
-    cl = train(presample_data, labels, false, false);
+    cl = train(presample_data(idxs,:), labels(idxs), false, false);
     
     n = n*100;
     
+    % Run Monte Carlo and filter out samples with trained classifier
     presample_data = zeros(360, n);
 	raw_samples = sample_gen(n, false);
     for j = 1:BATCH_SZ
 		presample_data(:,j) = reshape(raw_samples(60*(j-1)+1:60*(j-1)+60,:), 360, 1);
     end
-    labels = predict(cl, presample_data.');
+    labels = predict(cl, presample_data(idxs,:).');
     presample_data = presample_data(:, labels==1);
     batch_size = size(presample_data, 2);
     reshaped_data = zeros(60*batch_size, 6);
--- pruning.m
+++ pruning.m
@@ -1,15 +1,15 @@
-function [idxs] = pruning(presamples)
+function [idxs] = pruning(labels_new, presample_data_new)
 
     %Parameter Pruning using ReliefF
     %%
-    data = load(presamples);
+%     data = load(presamples);
 
     %% Presampling for pruning
-    index=find(data.labels_new);
+    index=find(labels_new);
     labels=ones(1,length(index));
-    presample_data=data.presample_data_new(:,index);
-    presample_data=[presample_data data.presample_data_new(:,1:10000)];
-    labels=[labels data.labels_new(1,1:10000)];
+    presample_data=presample_data_new(:,index);
+    presample_data=[presample_data presample_data_new(:,1:10000)];
+    labels=[labels labels_new(1,1:10000)];
 
     %% Run RELIEFF
     [ranks,weights]=relieff(transpose(presample_data),transpose(labels),100);
