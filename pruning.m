998ee1c34385d2b05496262bba9b12422d8addcc
yoojeenwoo <eugenewu0@gmail.com>
2018-05-26 15:11:14 -0700

    Refactored pruning as function

--- pruning.m
+++ pruning.m
@@ -1,15 +1,25 @@
-%Parameter Pruning using ReliefF
-clc; clear all; close all;
-%%
-load('concat_presamples.mat');
+function [idxs] = pruning(presamples)
 
-%% Presampling for pruning
-index=find(labels_new);
-labels=ones(1,length(index));
-presample_data=presample_data_new(:,index);
-presample_data=[presample_data presample_data_new(:,1:10000)];
-labels=[labels labels_new(1,1:10000)];
+    %Parameter Pruning using ReliefF
+    %%
+    data = load(presamples);
 
-%%   
-[ranks,weights]=relieff(transpose(presample_data),transpose(labels),100);
-plot(1:360,weights);
+    %% Presampling for pruning
+    index=find(data.labels_new);
+    labels=ones(1,length(index));
+    presample_data=data.presample_data_new(:,index);
+    presample_data=[presample_data data.presample_data_new(:,1:10000)];
+    labels=[labels data.labels_new(1,1:10000)];
+
+    %% Run RELIEFF
+    [ranks,weights]=relieff(transpose(presample_data),transpose(labels),100);
+
+    %% Show Predictor Ranks and Weights
+    bar(weights(ranks))
+    xlabel('Predictor rank')
+    ylabel('Predictor importance weight')
+
+    %% Prune
+    significant = sum(weights > 0);
+    idxs = ranks(1:significant);
+end
