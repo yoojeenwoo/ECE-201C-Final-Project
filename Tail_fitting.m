0f68c9506106a1ab40986313759688baecb59a24
yoojeenwoo <eugenewu0@gmail.com>
2018-05-26 10:33:25 -0700

    minor fixes to tail_fitting syntax

--- Tail_fitting.m
+++ Tail_fitting.m
@@ -1,27 +1,27 @@
 clc;
 clear all;
 close all;
-load twohundredthousand.mat
+load 5_25_results.mat
 %calculating the mean and variance of output delays
-mean_out_delay=mean(delay);
-sigma=(var(delay))^(1/2);
+mean_out_delay=mean(td);
+sigma=(var(td))^(1/2);
 %6 sigma
 thresh6=mean_out_delay+6*sigma;
 
 %fitting parameters for GPD
-paramEsts = gpfit(delay(delay>thresh6)-thresh6);
+paramEsts = gpfit(td(td>thresh6)-thresh6);
 kHat      = paramEsts(1)   % Tail index parameter
 sigmaHat  = paramEsts(2)   % Scale parameter
 x = linspace(0,100,1000);
 fit6sigma = gppdf(x,kHat,sigmaHat,0);
 
 %plotting the fit
-delay6thresh = delay(delay>thresh6);
+delay6thresh = td(td>thresh6);
 [nelements,centres] = hist(delay6thresh,100);
 figure
-plot(centres,nelements/length(delay),'o')
+plot(centres,nelements/length(td),'o')
 hold on
-plot(x/1e13+thresh6,fit6sigma/length(delay)*length(delay(delay>thresh6)),'-')
+plot(x/1e13+thresh6,fit6sigma/length(td)*length(td(td>thresh6)),'-')
 legend('Sample points','GPD Fitting Curve')
 hold off
 xlabel('Delay (s)')
